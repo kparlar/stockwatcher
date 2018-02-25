3c9edc76aae80f99fd7ccd2e9f0bf217142cb635
Koray Parlar <koray.parlar@ge.com>
2018-02-25 16:53:41 +0300

    updated GlobalControllerExceptionHandler added ExceptionMessage and StockWatcherException classes

--- services/services-stockwatcher/src/main/java/com/kparlar/stockwatcher/util/MessageCodeConstants.java
+++ services/services-stockwatcher/src/main/java/com/kparlar/stockwatcher/util/MessageCodeConstants.java
@@ -3,4 +3,8 @@ package com.kparlar.stockwatcher.util;
 public final class MessageCodeConstants {
     public static final String REST_CLIENT_EXCEPTION_MESSAGE = "org.springframework.web.client.RestClientException: Code: {}: id {}";
     public static final String REST_CLIENT_EXCEPTION_CODE = "EX01";
+
+    private MessageCodeConstants() {
+        throw new IllegalAccessError("Utility class");
+    }
 }
--- services/services-stockwatcher/src/main/java/com/kparlar/stockwatcher/util/StockWatcherConstantUtil.java
+++ services/services-stockwatcher/src/main/java/com/kparlar/stockwatcher/util/StockWatcherConstantUtil.java
@@ -3,4 +3,8 @@ package com.kparlar.stockwatcher.util;
 public final class StockWatcherConstantUtil {
 
     public static final String JSON_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+
+    private StockWatcherConstantUtil() {
+        throw new IllegalAccessError("Utility class");
+    }
 }
